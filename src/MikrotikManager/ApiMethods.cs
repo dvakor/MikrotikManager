ef91cf91b3aa2def7de8d5e7fea6e6cbd176f8ca
Дмитрий Королев <d.korolev@live.ru>
2022-05-22 14:51:36 +0700

    initial project

---
+++ src/MikrotikManager/ApiMethods.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MikrotikManager.Contracts;
+
+namespace MikrotikManager
+{
+    public static class ApiMethods
+    {
+        public static Task Index(HttpContext context)
+        {
+            context.Response.Redirect("./index.html");
+            return Task.CompletedTask;
+        }
+
+        public static async Task<DomainListDto> GetDomainList([FromServices] MikrotikService mikrotik)
+        {
+            return await mikrotik.GetDomainList();
+        }
+
+        public static async Task AddDomain(
+            HttpContext context,
+            [FromServices] MikrotikService mikrotik,
+            [FromQuery] string domain)
+        {
+            await mikrotik.AddDomain(domain);
+        }
+
+        public static async Task RemoveDomain(
+            [FromServices] MikrotikService mikrotik,
+            [FromQuery] string domain)
+        {
+            await mikrotik.RemoveDomain(domain);
+        }
+    }
+}
---
+++ src/MikrotikManager/Contracts/DomainListDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MikrotikManager.Contracts
+{
+    public class DomainListDto
+    {
+        public IEnumerable<string> Domains { get; set; }
+    }
+}
